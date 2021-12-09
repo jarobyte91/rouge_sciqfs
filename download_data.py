766925fcca70f49dda0bfa0fcfcfdfc9f1dbd65a
Juan <jarobyte91@gmail.com>
2021-12-08 22:25:04 -0400

    progress bar in download_data.py

--- download_data.py
+++ download_data.py
@@ -1,16 +1,29 @@
+# A simple downloader with progress bar
+
 import requests
+from tqdm import tqdm
 import zipfile
+from urllib.request import urlopen
 
-print("Downloading data.zip...")
-url = "https://web.cs.dal.ca/~juanr/downloads/data.zip"
-data = requests.get(url)
+url = "https://web.cs.dal.ca/~juanr/downloads/malnis_dataset.zip"
+block_size = 1024 #1 Kibibyte
 
-with open("data.zip", "wb") as file:
-    file.write(data.content)
+filename = url.split("/")[-1]
+print(f"Downloading {filename}...")
+site = urlopen(url)
+meta = site.info()
+# Streaming, so we can iterate over the response.
+response = requests.get(url, stream=True)
+total_size_in_bytes = int(meta["Content-Length"])
+progress_bar = tqdm(total = total_size_in_bytes, unit='iB', unit_scale=True)
+with open(filename, 'wb') as file:
+    for data in response.iter_content(block_size):
+        progress_bar.update(len(data))
+        file.write(data)
+progress_bar.close()
 print("Download complete")
-
-print("Extracting data.zip...")
-zip = zipfile.ZipFile("data.zip", "r")
+print(f"Extracting {filename}...")
+zip = zipfile.ZipFile(filename, "r")
 zip.extractall()
 zip.close()
 print("Extracting complete")
