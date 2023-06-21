a3d65caaa1d6033deb892829af3622aefd73bc9c
Juan Antonio Ramirez-Orta <jarobyte91@gmail.com>
2023-06-21 13:02:08 -0400

    new names for notebooks

--- connection.sh
+++ connection.sh
@@ -1,2 +1,1 @@
-#!/bin/sh
-salloc --time=3:0:0 --ntasks=1 --cpus-per-task=4 --mem=64G --account=rrg-emilios srun /home/jarobyte/envs/malnis/bin/notebook.sh
+salloc --time=3:0:0 --ntasks=1 --cpus-per-task=2 --mem=16G --gres=gpu:1 --account=def-emilios srun $VIRTUAL_ENV/bin/notebook.sh
